016f7c6e55e34bdde04a616d42bd475cdc91d907
hafidz313 <lefet313@gmail.com>
2022-03-28 10:45:28 +0700

    first commit

---
+++ pratikum5/lib/widget/listview.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'card.dart';
+import 'pratikum5/widget/card.dart';
+import '../model/item.dart';
+
+class MyListView extends StatelessWidget {
+  const MyListView({
+    Key? key,
+    required this.items,
+    required this.page,
+  }) : super(key: key);
+
+  final List<Item> items;
+  final String page;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, page, arguments: item);
+          },
+          child: MyCard(
+            name: item.name,
+            price: item.price,
+            weight: item.weight,
+          ),
+        );
+      },
+    );
+  }
+}
